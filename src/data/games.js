5f7250d8dab6585627af2d215c5002f1dc626c8c
andrew-brainerd <drwb333@gmail.com>
2020-03-11 16:52:02 -0400

    Remove unnecessary then

--- src/data/games.js
+++ src/data/games.js
@@ -3,15 +3,15 @@ const log = require('../utils/log');
 const { DUNGEONS_COLLECTION } = require('../constants/collections');
 
 const createGame = async (name, createdBy) => {
-  const newGame = await data
-    .insertOne(DUNGEONS_COLLECTION, {
+  const newGame = await data.insertOne(
+    DUNGEONS_COLLECTION, {
       name,
       createdBy,
       members: [createdBy]
-    })
-    .then(inserted => inserted);
+    });
 
   log.success(`Created new game ${newGame.name} (${newGame._id})`);
+
   return newGame;
 };
 
