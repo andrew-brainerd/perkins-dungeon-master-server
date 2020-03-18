870ae04c978b2bacb6181a2869fff88036b5ee3d
Andrew Brainerd <drwb333@gmail.com>
2020-03-17 20:11:09 -0400

    Add get characters response

--- src/data/games.js
+++ src/data/games.js
@@ -1,16 +1,18 @@
+const { v1: uuidv1 } = require('uuid');
+const { isEmpty } = require('ramda');
 const data = require('../utils/data');
 const log = require('../utils/log');
 const { pusher } = require('../utils/pusher');
 const { UPDATE_GAME } = require('../constants/pusher');
 const { GAMES_COLLECTION } = require('../constants/collections');
-const { parseUserInput } = require('../utils/game');
+const { AUTH_USER, GAME_MASTER } = require('../constants/game');
 
 const createGame = async (name, createdBy) => {
   const newGame = await data.insertOne(
     GAMES_COLLECTION, {
-      name,
-      createdBy,
-      members: [createdBy]
+    name,
+    createdBy,
+    members: [createdBy]
   });
 
   log.success(`Created new game ${newGame.name} (${newGame._id})`);
@@ -26,14 +28,19 @@ const getGame = async gameId => {
   return await data.getById(GAMES_COLLECTION, gameId);
 };
 
+const getGameCharacters = async gameId => {
+  const game = await getGame(gameId);
+  const characters = ((game || {}).characters) || [];
+
+  return characters;
+};
+
 const addLog = async (gameId, message) => {
-  const serverResponse = parseUserInput(message);
+  const serverResponse = await parseUserInput(message);
   const appendUserMessage = await data.updateOne(GAMES_COLLECTION, gameId, message);
   const appendServerMessage = await data.updateOne(GAMES_COLLECTION, gameId, serverResponse);
 
   pusher.trigger(gameId, UPDATE_GAME, { appendUserMessage, appendServerMessage });
-
-  console.log('Response: %o', serverResponse);
 
   return {
     gameId,
@@ -43,9 +50,71 @@ const addLog = async (gameId, message) => {
   };
 };
 
+const getUniqueMessage = message => ({
+  messages: {
+    id: uuidv1(),
+    timestamp: new Date(),
+    ...message
+  }
+});
+
+const parseUserInput = async userInput => {
+  const { messages: { gameId, message } } = userInput || {};
+
+  if (message === 'login' || message === 'signin') {
+    return getUniqueMessage({
+      ...AUTH_USER,
+      message: message.isAuthenticated ? 'Already signed in :D' : 'Signing In...'
+    });
+  } else if (message === 'logout' || message === 'signout') {
+    return getUniqueMessage({
+      ...AUTH_USER,
+      message: message.isAuthenticated ? 'Signing Out...' : 'Not signed in'
+    });
+  } else if (message === 'newgame') {
+    if (message.isAuthenticated) {
+      return getUniqueMessage({
+        ...GAME_MASTER,
+        message: 'Starting a new game...'
+      });
+    } else {
+      return getUniqueMessage({
+        ...AUTH_USER,
+        message: 'Please sign in first'
+      });
+    }
+  } else if (message === 'char' || message === 'characters') {
+    const characters = await getGameCharacters(gameId);
+
+    return getUniqueMessage({
+      ...GAME_MASTER,
+      message: !isEmpty(characters) ?
+        `Game Characters:
+        <pre>
+        ${characters.map(({ name }) =>
+          `<span>Name: </span><span>${name}</span>`)
+        }
+        </pre>` :
+        'No characters in this game yet'
+    });
+  } else if (message === 'newCharacter') {
+    return getUniqueMessage({
+      ...GAME_MASTER,
+      message: 'Create A New Character',
+      requiresUserInput: true
+    });
+  }
+
+  return getUniqueMessage({
+    ...GAME_MASTER,
+    message: 'Unrecognized user input'
+  });
+};
+
 module.exports = {
   createGame,
   getGames,
   getGame,
+  getGameCharacters,
   addLog
 };
