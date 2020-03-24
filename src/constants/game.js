0b57abf89f1e06211b1c50f4b54a39dd07ec75d5
Andrew Brainerd <drwb333@gmail.com>
2020-03-24 13:52:08 -0400

    Character Creation v2 (#16)
    * Add character definition constants
    
    * Simplify lint command

--- src/constants/game.js
+++ src/constants/game.js
@@ -10,8 +10,93 @@ const GAME_MASTER = {
 
 const UPDATE_GAME = 'updateGame';
 
+const CHARACTER_CLASSES = [
+  'barbarian',
+  'bard',
+  'cleric',
+  'druid',
+  'fighter',
+  'monk',
+  'paladin',
+  'ranger',
+  'rogue',
+  'rorcerer',
+  'warlock',
+  'wizard'
+];
+
+const CHARACTER_RACES = [
+  'dwarf',
+  'elf',
+  'halfling',
+  'human',
+  'dragonborn',
+  'gnome',
+  'halfElf',
+  'halfOrc',
+  'tiefling'
+];
+
+const CHARACTER_ALIGNMENTS = [
+  'lawfulGood',
+  'neutralGood',
+  'chaoticGood',
+  'lawfulNeutral',
+  'neutral',
+  'chaoticNeutral',
+  'lawfulEvil',
+  'neutralEvil',
+  'chaoticEvil'
+];
+
+const CHARACTER_SKIN_COLORS = [
+  'Pale',
+  'Fair',
+  'Light',
+  'Light Tan',
+  'Tan',
+  'Dark Tan',
+  'Brown',
+  'Dark Brown',
+  'Black',
+  'Gray',
+  'White',
+  'Green',
+  'Blue',
+  'Purple'
+];
+
+const CHARACTER_HAIR_COLORS = [
+  'Black',
+  'Gray',
+  'Platinum',
+  'White',
+  'Dark Blonde',
+  'Blonde',
+  'Bleach Blonde',
+  'Dark Redhead',
+  'Redhead',
+  'Light Redhead',
+  'Brunette',
+  'Auburn',
+  'Yellow',
+  'Amber',
+  'Brown',
+  'Hazel',
+  'Green',
+  'Blue',
+  'Aqua',
+  'Red',
+  'Purple'
+];
+
 module.exports = {
   AUTH_USER,
   GAME_MASTER,
-  UPDATE_GAME
+  UPDATE_GAME,
+  CHARACTER_CLASSES,
+  CHARACTER_RACES,
+  CHARACTER_ALIGNMENTS,
+  CHARACTER_SKIN_COLORS,
+  CHARACTER_HAIR_COLORS
 };
--- src/routes/api/characters.js
+++ src/routes/api/characters.js
@@ -3,18 +3,34 @@ const characters = require('express').Router();
 const charactersData = require('../../data/characters');
 const status = require('../../constants/statusMessages');
 const { validator } = require('../../utils/validator');
+const {
+  CHARACTER_CLASSES,
+  CHARACTER_RACES,
+} = require('../../constants/game');
 
 const postCharacterBody = Joi.object({
   gameId: Joi.string().required(),
+  playerId: Joi.string().required(),
   name: Joi.string().required(),
-  createdBy: Joi.string().required()
+  class: Joi.string().valid(CHARACTER_CLASSES).required(),
+  race: Joi.string().valid(CHARACTER_RACES).required()
 });
 
 characters.post('/', validator.body(postCharacterBody), async (req, res) => {
-  const { body: { gameId, name, createdBy } } = req;
+  const { body: { createdBy, ...attributes } } = req;
 
-  const newCharacter = await charactersData.createCharacter({ gameId, name, createdBy });
-  if (!newCharacter) return status.serverError(res, 'Failed', `Failed to create character [${name}]`);
+  const character = {
+    ...attributes,
+    level: 1,
+    background: 'Acolyte',
+    location: 'startLocationGuid',
+    playerId: createdBy,
+    experiencePoints: 0,
+    remainingSpells: 3
+  };
+
+  const newCharacter = await charactersData.createCharacter(character);
+  if (!newCharacter) return status.serverError(res, 'Failed', `Failed to create character [${character.name}]`);
 
   return status.created(res, { ...newCharacter });
 });
