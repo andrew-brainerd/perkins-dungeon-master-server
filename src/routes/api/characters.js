a1f7223344b7b78b537c16b1f7a3dc0096f2d0b1
andrew-brainerd <drwb333@gmail.com>
2020-03-25 19:24:49 -0400

    Bump gm-common version

--- src/routes/api/characters.js
+++ src/routes/api/characters.js
@@ -1,6 +1,6 @@
 const Joi = require('joi');
 const characters = require('express').Router();
-const { characterConstants } = require('gm-common');
+const { characters: characterDefinitions } = require('gm-common');
 const charactersData = require('../../data/characters');
 const status = require('../../utils/statusMessages');
 const { validator } = require('../../utils/validator');
@@ -9,12 +9,12 @@ const postCharacterBody = Joi.object({
   gameId: Joi.string().required(),
   playerId: Joi.string().required(),
   name: Joi.string().required(),
-  class: Joi.string().valid(characterConstants.classTypes).required(),
-  race: Joi.string().valid(characterConstants.raceTypes).required()
+  class: Joi.string().valid(characterDefinitions.classTypes).required(),
+  race: Joi.string().valid(characterDefinitions.raceTypes).required()
 });
 
-console.log('Class Types: %o', characterConstants.classTypes);
-console.log('Race Types: %o', characterConstants.raceTypes);
+console.log('Class Types: %o', characterDefinitions.classTypes);
+console.log('Race Types: %o', characterDefinitions.raceTypes);
 
 characters.post('/', validator.body(postCharacterBody), async (req, res) => {
   const { body: { createdBy, ...attributes } } = req;
