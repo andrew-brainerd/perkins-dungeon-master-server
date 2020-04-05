fd0a39fce370b5456a7cdc831ef4874b1f5cc559
Alex Bowen <ajbowen249@gmail.com>
2020-04-05 13:58:08 -0400

    Accept and validate abilities for new characters

--- src/api/validation/characters.js
+++ src/api/validation/characters.js
@@ -1,6 +1,9 @@
 const Joi = require('joi');
 const { characters } = require('gm-common');
 
+// Validation range is worst possible roll to best possible roll
+const abilityScore = Joi.number().min(3).max(18);
+
 const postCharacterBody = Joi.object({
   gameId: Joi.string().required(),
   playerId: Joi.string().required(),
@@ -8,7 +11,15 @@ const postCharacterBody = Joi.object({
   class: Joi.string().valid(characters.classTypes).required(),
   race: Joi.string().valid(characters.raceTypes).required(),
   order: Joi.string().valid(characters.alignments.orderTypes).required(),
-  morality: Joi.string().valid(characters.alignments.moralityTypes).required()
+  morality: Joi.string().valid(characters.alignments.moralityTypes).required(),
+  abilityScores: Joi.object({
+    strength: abilityScore.required(),
+    dexterity: abilityScore.required(),
+    constitution: abilityScore.required(),
+    intelligence: abilityScore.required(),
+    wisdom: abilityScore.required(),
+    charisma: abilityScore.required()
+  }).required()
 });
 
 const getGameCharactersQuery = Joi.object({
